ce7e26abd67fedb11f00e36bec8dfc93a1839f2c
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-11-23 10:54:50 +0300

    added assests

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 const App = () => {
   return <main>
-    <h1 className="text-5xl text-oranged-500 font-bold">Welcome to Awwwards</h1>
+    <h1 className="text-5xl text-orange-500 font-bold">Welcome to Awwwards</h1>
   </main>;
 };
 
